fca35f3b7bb6fe4aded1713d5a1ae97d629d2490
Edun Omatseye <edunomatseye@yahoo.com>
2020-11-28 17:15:17 +0100

    Fix: returned the count value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {};
+    state = {
+        count: 0,
+        age: 34,
+        address: {
+            street: 'okoro street'
+        }
+    };
+
     render() {
-        return <div><h1>Hello world is rocking...</h1><button>Increment</button></div>
+        return (
+            <div>
+                <div>Hello world</div>
+                <span>{this.state.count}</span>
+                <button>Increment</button>
+            </div>
+        );
     }
 }
 
