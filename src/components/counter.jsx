297c46271a761f6ac5ca22ef01610ce8a0f69bcf
Edun Omatseye <edunomatseye@yahoo.com>
2020-11-28 16:59:49 +0100

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {};
+    render() {
+        return <div><h1>Hello world is rocking...</h1><button>Increment</button></div>
+    }
+}
+
+export default Counter;
