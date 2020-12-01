296060cbd76b5b98122f69035dc1531dbb518844
Edun Omatseye <edunomatseye@yahoo.com>
2020-12-01 11:57:02 +0100

    Feature: Increment, Decrement, Delete and Reset Button addede successfully.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        age: 34,
-        address: {
-            street: 'okoro street'
-        }
-    };
+  
+
+    styles = {
+        fontSize: 15,
+        fontWeight: 'bold'
+    }
 
     render() {
-        return (
+        return(
             <div>
-                <div>Hello world</div>
-                <span>{this.state.count}</span>
-                <button>Increment</button>
+                <span className={ this.getBadgeclasses() }>{this.formatCount()}</span>
+                <button onClick={ this.props.onHandleIncrement } className="btn btn-secondary btn-sm m-2">+</button>
+                <button onClick={ this.props.onHandleDecrement } className="btn btn-secondary btn-sm m-2">-</button>
+                <button onClick={ this.props.onDelete } className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={ this.props.onHandleReset } className="btn btn-secondary btn-sm m-2">Reset</button>
+
             </div>
         );
+    }
+
+    
+
+    getBadgeclasses() {
+        const { value } = this.props.counter;
+        let classes = "badge m-2 badge-";
+        classes += value === 0 ? "warning": (value > 0) ? "success": "danger";
+        return classes
+    }
+
+    formatCount() {
+        const { value } = this.props.counter;
+        return (value === 0) ? 'Zero': +value;
+        //return this.state.value ?? 'Zero'
     }
 }
 
